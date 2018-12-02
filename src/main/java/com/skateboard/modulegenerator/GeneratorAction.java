dcdd961c90ee550cd222bc9864b52365687ae848
haoxuan.wu <haoxuan.wu@neulion.com.cn>
2018-12-02 19:44:00 +0800

    add foundation

---
+++ src/main/java/com/skateboard/modulegenerator/GeneratorAction.java
@@ -0,0 +1,233 @@
+package com.skateboard.modulegenerator;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+import java.io.*;
+
+
+public class GeneratorAction extends AnAction {
+
+
+    private final String LIBS_DIR = "libs";
+
+    private final String SRC_DIR = "src";
+
+    private final String MAIN_DIR = "main";
+
+    private final String JAVA_DIR = "java";
+
+    private final String MANIFEST_DIR = "manifest";
+
+    private final String MANIFEST_FILE = "AndroidManifest.xml";
+
+    private final String RES_DIR = "res";
+
+    private final String DRAWABLE_DIR = "drawable";
+
+    private final String LAYOUT_DIR = "layout";
+
+    private final String VALUES_DIR = "values";
+
+    private final String GITGNORE_FILE = ".gitgnore";
+
+    private final String BUILD_FILE = "build.gradle";
+
+    private final String GRADLE_PROPERTIES_FILE = "gradle.properties";
+
+    private final String PROGUARD_RULES_FILE = "proguard-rules.pro";
+
+    private final String GRADLE_PROPERTIES_CONTENT = "isRunAlone=false";
+
+    private final String MANIFEST_CONTENT = "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+            "    package=$packageName>\n" +
+            "\n" +
+            "</manifest>";
+
+
+    private final String GRADLE_CONTENT = "if (isRunAlone.toBoolean()) {\n" +
+            "    apply plugin: 'com.android.application'\n" +
+            "} else {\n" +
+            "    apply plugin: 'com.android.library'\n" +
+            "}\n" +
+            "\n" +
+            "apply plugin: 'kotlin-android'\n" +
+            "\n" +
+            "apply plugin: 'kotlin-android-extensions'\n" +
+            "\n" +
+            "apply plugin: 'kotlin-kapt'\n" +
+            "\n" +
+            "android {\n" +
+            "    compileSdkVersion 28\n" +
+            "\n" +
+            "    defaultConfig {\n" +
+            "        if (isRunAlone.toBoolean()) {\n" +
+            "            applicationId \"$packageName\"\n" +
+            "        }\n" +
+            "        minSdkVersion 15\n" +
+            "        targetSdkVersion 28\n" +
+            "        versionCode 1\n" +
+            "        versionName \'1.0\'\n" +
+            "        testInstrumentationRunner \"androidx.test.runner.AndroidJUnitRunner\"\n" +
+            "    }\n" +
+            "\n" +
+            "    buildTypes {\n" +
+            "        release {\n" +
+            "            minifyEnabled false\n" +
+            "            proguardFiles getDefaultProguardFile('proguard-android.txt'), 'proguard-rules.pro'\n" +
+            "        }\n" +
+            "    }\n" +
+            "\n" +
+            "    sourceSets {\n" +
+            "        main {\n" +
+            "            if (isRunAlone.toBoolean()) {\n" +
+            "                manifest.srcFile 'src/main/manifest/AndroidManifest.xml'\n" +
+            "            } else {\n" +
+            "                manifest.srcFile 'src/main/AndroidManifest.xml'\n" +
+            "            }\n" +
+            "        }\n" +
+            "    }\n" +
+            "\n" +
+            "    kapt {\n" +
+            "        arguments {\n" +
+            "            arg(\"AROUTER_MODULE_NAME\", project.getName())\n" +
+            "        }\n" +
+            "    }\n" +
+            "\n" +
+            "    compileOptions {\n" +
+            "        sourceCompatibility JavaVersion.VERSION_1_8\n" +
+            "        targetCompatibility JavaVersion.VERSION_1_8\n" +
+            "    }\n" +
+            "\n" +
+            "}\n" +
+            "\n" +
+            "dependencies {\n" +
+            "    implementation fileTree(dir: 'libs', include: ['*.jar'])\n" +
+            "}\n";
+
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+        ModuleInfoDialog.pop(new ModuleInfoDialog.DialogCallback() {
+            @Override
+            public void onOkClicked(String moduleName, String packageName) {
+                generateModuleFiles(moduleName, packageName, e.getProject().getBasePath());
+                e.getProject().getBaseDir().refresh(true, true);
+
+            }
+
+            @Override
+            public void onCancelClicked() {
+
+            }
+        });
+
+    }
+
+    private void generateModuleFiles(String moduleName, String packageName, String basePath) {
+        String modulePath = createModuleDir(basePath, moduleName);
+        createLibDir(modulePath);
+        createSrc(modulePath, packageName);
+        createBuildFiles(modulePath, packageName);
+    }
+
+
+    private String createDir(String path, String fileName) {
+
+        File file = new File(path, fileName);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        return file.getPath();
+    }
+
+    private String createFile(String path, String name) {
+
+        File file = new File(path, name);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file.getPath();
+    }
+
+
+    private String createModuleDir(String path, String name) {
+
+        return createDir(path, name);
+    }
+
+    private void createLibDir(String modulePath) {
+
+        createDir(modulePath, LIBS_DIR);
+    }
+
+    private void createSrc(String modulePath, String packageName) {
+
+        String srcPath = createDir(modulePath, SRC_DIR);
+        String mainPath = createDir(srcPath, MAIN_DIR);
+        String javaPath = createDir(mainPath, JAVA_DIR);
+
+        createDir(javaPath, packageName.replace(".", File.separator));
+
+        createManifestFile(mainPath, packageName);
+
+        createRes(mainPath);
+    }
+
+    private void createManifestFile(String mainPath, String packageName) {
+
+        String manifestPath = createDir(mainPath, MANIFEST_DIR);
+        String manifestFile=createFile(manifestPath, MANIFEST_FILE);
+        String outerManifestFile=createFile(mainPath,MANIFEST_FILE);
+        writeContentToFile(MANIFEST_CONTENT.replace("$packageName", "\"" + packageName + "\""), outerManifestFile);
+        writeContentToFile(MANIFEST_CONTENT.replace("$packageName", "\"" + packageName + "\""), manifestFile);
+    }
+
+    private void createRes(String mainPath) {
+        String resPath = createDir(mainPath, RES_DIR);
+        createDir(resPath, DRAWABLE_DIR);
+        createDir(resPath, LAYOUT_DIR);
+        createDir(resPath, VALUES_DIR);
+    }
+
+
+    private void createBuildFiles(String modulePath, String packageName) {
+        createFile(modulePath, GITGNORE_FILE);
+        createFile(modulePath, PROGUARD_RULES_FILE);
+        createBuildFile(modulePath, packageName);
+        createGradlePropertiesFile(modulePath);
+    }
+
+    private void createBuildFile(String modulePath, String packageName) {
+        String buildFile = createFile(modulePath, BUILD_FILE);
+        writeContentToFile(GRADLE_CONTENT.replace("$packageName", packageName), buildFile);
+    }
+
+    private void createGradlePropertiesFile(String modulePath) {
+
+        String propertiesFile = createFile(modulePath, GRADLE_PROPERTIES_FILE);
+        writeContentToFile(GRADLE_PROPERTIES_CONTENT, propertiesFile);
+
+    }
+
+    private void writeContentToFile(String content, String filePath) {
+
+
+        try {
+            BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath)));
+            fw.write(content);
+            fw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/skateboard/modulegenerator/ModuleInfoDialog.java
@@ -0,0 +1,148 @@
+package com.skateboard.modulegenerator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.GridBagConstraints.*;
+
+
+public class ModuleInfoDialog extends JDialog {
+
+    private DialogCallback dialogCallback;
+
+    private final static String OK = "OK";
+
+    private final static String CANCEL = "CANCEL";
+
+    private final static String TITLE = "创建新module";
+
+    private final static String MODULE_NAME_HINT = "请输入模块名称:";
+
+    private final static String PACKAGE_NAME_HINT = "请输入包名:";
+
+    private final static String ERROR_MESSAGE = "模块名和包名不能为空";
+
+    public ModuleInfoDialog(DialogCallback dialogCallback) {
+        setSize(450, 300);
+        setModal(true);
+        setTitle(TITLE);
+        prepareUI();
+        this.dialogCallback = dialogCallback;
+
+    }
+
+    private void prepareUI() {
+
+
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        JPanel contentPannel = new JPanel();
+        contentPannel.setLayout(gridBagLayout);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.weightx = 0;
+
+
+        JLabel moduleHintLabel = new JLabel();
+        moduleHintLabel.setText(MODULE_NAME_HINT);
+        contentPannel.add(moduleHintLabel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.weightx=0;
+        JLabel packageNameHintLabel = new JLabel();
+        packageNameHintLabel.setText(PACKAGE_NAME_HINT);
+        contentPannel.add(packageNameHintLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.weightx=1;
+        constraints.fill=GridBagConstraints.HORIZONTAL;
+        JTextField moduleNameImp = new JTextField();
+        moduleNameImp.setForeground(Color.WHITE);
+        contentPannel.add(moduleNameImp, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.weightx=1;
+        constraints.fill=GridBagConstraints.HORIZONTAL;
+        JTextField packageNameImp = new JTextField();
+        packageNameImp.setForeground(Color.WHITE);
+        contentPannel.add(packageNameImp, constraints);
+
+
+        JLabel errorLabel = new JLabel();
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setVisible(false);
+
+
+        JButton okBtn = new JButton();
+        okBtn.setText(OK);
+        okBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (moduleNameImp.getText().isEmpty() || packageNameImp.getText().isEmpty()) {
+                    errorLabel.setVisible(true);
+                    errorLabel.setText(ERROR_MESSAGE);
+                    return;
+                }
+                if (dialogCallback != null) {
+                    dialogCallback.onOkClicked(moduleNameImp.getText(), packageNameImp.getText());
+                }
+                dispose();
+            }
+        });
+
+
+        JButton cancelBtn = new JButton();
+        cancelBtn.setText(CANCEL);
+        cancelBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (dialogCallback != null) {
+                    dialogCallback.onCancelClicked();
+                }
+                dispose();
+            }
+        });
+
+        constraints.anchor=WEST;
+        constraints.weightx=0;
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        contentPannel.add(okBtn, constraints);
+
+        constraints.fill=GridBagConstraints.NONE;
+        constraints.gridy = 2;
+        constraints.gridx = 1;
+        contentPannel.add(cancelBtn, constraints);
+
+        constraints.gridy = 3;
+        constraints.gridx = 0;
+        contentPannel.add(errorLabel, constraints);
+
+        setContentPane(contentPannel);
+    }
+
+
+    interface DialogCallback {
+
+        void onOkClicked(String moduleName, String packageName);
+
+        void onCancelClicked();
+    }
+
+
+    public static void pop(DialogCallback callback) {
+        ModuleInfoDialog infoDialog = new ModuleInfoDialog(callback);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        infoDialog.setLocation(screenSize.width / 2 - infoDialog.getWidth() / 2, screenSize.height / 2 - infoDialog.getHeight() / 2);
+        infoDialog.setVisible(true);
+    }
+
+}
